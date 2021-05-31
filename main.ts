da0371eadde6c4a0ba937e0de27db71424845f8d
maria1412 <82562294+maria1412@users.noreply.github.com>
2021-05-31 11:41:11 +0900

    MakeCodeプロジェクトの初期ファイル

---
+++ main.ts
@@ -0,0 +1,65 @@
+input.onGesture(Gesture.TiltRight, function () {
+    for (let index = 0; index < 4; index++) {
+        for (let a = 0; a <= 2; a++) {
+            led.plot(4 + a, 1)
+            led.plot(4 + a, 3)
+            led.plot(3 + a, 0)
+            led.plot(3 + a, 4)
+            led.plot(2 + a, 2)
+            led.plot(1 + a, 1)
+            led.plot(1 + a, 3)
+            led.plot(0 + a, 0)
+            led.plot(0 + a, 4)
+            led.plot(-1 + a, 2)
+            led.plot(-2 + a, 1)
+            led.plot(-2 + a, 3)
+            basic.pause(500)
+            led.unplot(4 + a, 1)
+            led.unplot(4 + a, 3)
+            led.unplot(3 + a, 0)
+            led.unplot(3 + a, 4)
+            led.unplot(2 + a, 2)
+            led.unplot(1 + a, 1)
+            led.unplot(1 + a, 3)
+            led.unplot(0 + a, 0)
+            led.unplot(0 + a, 4)
+            led.unplot(-1 + a, 2)
+            led.unplot(-2 + a, 1)
+            led.unplot(-2 + a, 3)
+        }
+    }
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    for (let index = 0; index < 4; index++) {
+        for (let a = 0; a <= 2; a++) {
+            led.plot(0 - a, 1)
+            led.plot(0 - a, 3)
+            led.plot(1 - a, 0)
+            led.plot(1 - a, 4)
+            led.plot(2 - a, 2)
+            led.plot(3 - a, 1)
+            led.plot(3 - a, 3)
+            led.plot(4 - a, 0)
+            led.plot(4 - a, 4)
+            led.plot(5 - a, 2)
+            led.plot(6 - a, 1)
+            led.plot(6 - a, 3)
+            basic.pause(500)
+            led.unplot(0 - a, 1)
+            led.unplot(0 - a, 3)
+            led.unplot(1 - a, 0)
+            led.unplot(1 - a, 4)
+            led.unplot(2 - a, 2)
+            led.unplot(3 - a, 1)
+            led.unplot(3 - a, 3)
+            led.unplot(4 - a, 0)
+            led.unplot(4 - a, 4)
+            led.unplot(5 - a, 2)
+            led.unplot(6 - a, 1)
+            led.unplot(6 - a, 3)
+        }
+    }
+})
+basic.forever(function () {
+	
+})
